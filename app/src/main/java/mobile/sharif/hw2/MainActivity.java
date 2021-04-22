f988285d1a44a43132fb9499e256424bbd8dc87e
aliraad79 <aliraad79@gmail.com>
2021-04-22 16:16:27 +0430

    start of map view

--- app/src/main/java/mobile/sharif/hw2/MainActivity.java
+++ app/src/main/java/mobile/sharif/hw2/MainActivity.java
@@ -6,9 +6,34 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private MapView mapView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Mapbox.getInstance(this, getString(R.string.mapbox_access_token));
+
         setContentView(R.layout.activity_main);
+
+        mapView = (MapView) findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(@NonNull MapboxMap mapboxMap) {
+
+                mapboxMap.setStyle(Style.MAPBOX_STREETS, new Style.OnStyleLoaded() {
+                    @Override
+                    public void onStyleLoaded(@NonNull Style style) {
+
+                        // Map is set up and the style has loaded. Now you can add data or make other map adjustments
+
+
+                    }
+                });
+
+            }
+        });
     }
+
 }
